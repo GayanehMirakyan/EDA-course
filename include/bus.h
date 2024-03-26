113249ecf5bd5be594c8353b94b779d115ef733e
Gayane <gayane.mirakyan@ispras.ru>
2024-03-26 12:17:33 +0400

    Add tmp impl

---
+++ include/bus.h
@@ -0,0 +1,8 @@
+//
+// Created by gayane on 3/26/24.
+//
+
+#ifndef EDA_COURSE_INCLUDE_MAIN_H
+#define EDA_COURSE_INCLUDE_MAIN_H
+
+#endif //EDA_COURSE_INCLUDE_MAIN_H
---
+++ src/bus.cpp
@@ -0,0 +1,3 @@
+//
+// Created by gayane on 3/26/24.
+//
